9e1b6c870ee90c5a0aab14cf0bbcf2a2792db924
carinadidomenico <carinadidomenico@gmail.com>
2019-05-13 17:00:52 -0300

    Exercício força de senha

---
+++ 01/js/script.js
@@ -0,0 +1,25 @@
+// javascript
+// var li = document.querySelectorAll ('li');
+
+// for (var i = 0; i < li.length; i++){
+//     li[i].textContent = "gatinho";
+// }
+
+// var btn = document.querySelectorAll ('button');
+
+// for (var i = 0; i < btn.length; i++){
+//     btn[i].textContent = "gatinho";
+// }
+
+// $('button').text ("gatíneo");
+
+// $('.teste').text("Paranapiacaba")
+
+// function mudarTextoLi () {
+//     $('li').text("Rabicho");
+// }
+
+// $('document').ready(function(){
+//     alert ("Carregoy");
+//     mudarTextoLi ();
+// })
