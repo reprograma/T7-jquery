146103f4d057bd764a463d5ddba51ead661856d8
Lydia Rodrigues <mlydia295@gmail.com>
2019-05-13 14:55:22 -0300

    [AULA1] - Início Jquery

--- 01/js/script.js
+++ 01/js/script.js
@@ -1,22 +1,26 @@
-// Javascript
+// Código de demonstração em Javascript
 // var lis = document.querySelectorAll('li');
 // for(var i = 0; i < lis.length ; i++){
 //     lis[i].textContent = "BATATA";
 // }
 
-// $(function(){
-//     alert("Carregou");
-//     console.log("Carregou !");
-// })
 
 
+//Código em jQuery
 function mudaTextoLi(){
-    if($('li').length > 0 ){
-        console.log($('.lista1 .botao').text());
-    }
+    
+    $('li').text("BATATA");
+    // if($('li').length > 0 ){
+    //     console.log($('.lista1 .botao').text());
+    // }
 }
 
 $(document).ready(function(){
     mudaTextoLi();
 });
 
+//Função que faz a mesma coisa que o .ready.
+// $(function(){
+//     alert("Carregou");
+//     console.log("Carregou !");
+// })
